540896764fcb328ebf87549b683271180434dc0f
Oleh Bilyk <sojer90@gmail.com>
2021-12-22 21:12:06 +0200

    a

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 
+
 let project_folder = "dist";
 let source_folder = "#src";
 
@@ -13,7 +14,7 @@ let path = {
   },
   src: {
     html: source_folder + "/*.html",
-    css: source_folder + "/scss/style.css",
+    css: source_folder + "/scss/style.scss",
     js: source_folder + "/js/script.js",
     img: source_folder + "/img/**/*.{jpg ,png ,svg ,gif ,ico ,webp}",
     fonts: source_folder + "/fonts/*.ttf",
@@ -38,7 +39,9 @@ let {src, dest} = require('gulp'),
   group_media = require("gulp-group-css-media-queries"),
   clean_css = require("gulp-clean-css"),
   rename = require("gulp-rename"),
-  uglify = require("gulp-uglify-es").default;
+  uglify = require("gulp-uglify-es").default,
+  imagemin = require("gulp-imagemin");
+  webp = require("gulp-webp");
 
 function browserSync () {
   browsersync.init({
@@ -101,20 +104,49 @@ function js() {
     .pipe(browsersync.stream())
 }
 
+function images() {
+  return src(path.src.img)
+    .pipe(
+      webp ({
+        quality: 70
+      })
+    )
+    .pipe(dest(path.build.img))
+    .pipe(src(path.src.img))
+    .pipe(
+      imagemin({
+        progressive: true,
+        svgoPlugins: [{ removeViewBox: false }],
+        interlaced: true,
+        optimizationLevel: 3,
+      })
+    )
+    .pipe(fileinclude())
+    .pipe(dest(path.build.img))
+    .pipe(browsersync.stream())
+}
+
+function images() {
+  return src(path.src.img)
+    .pipe(dest(path.build.img))
+    .pipe(browsersync.stream())
+}
+
 function watchFiles() {
   gulp.watch([path.watch.html],html);
   gulp.watch([path.watch.css],css);
   gulp.watch([path.watch.js],js);
+  gulp.watch([path.watch.img],images);
 }
 
 function clean() {
   return del(path.clean);
 }
 
-let build = gulp.series(clean, gulp.parallel(js, css, html));
+let build = gulp.series(clean, gulp.parallel(js, css, html, images));
 let watch = gulp.parallel(build, watchFiles, browserSync);
 
-
+exports.images = images;
 exports.js = js;
 exports.css = css;
 exports.html = html;
